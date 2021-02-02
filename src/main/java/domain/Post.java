451ca8ae9d3f1aad3cf4ecb06540fbf2f8a198b6
Alex Pahomov <renger29@gmail.com>
2021-02-02 19:54:35 +0300

    commit v1

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Post {
+    private String profileName;
+    private String reportButton;
+    private String date;
+    private String postLogoURL;
+    private String mainText;
+    private String imageURL;
+    private String likesCount;
+    private String subscriberInfo
+    private String commentButton;
+    private String repostButton;
+    private String viewsCount;
+    }
