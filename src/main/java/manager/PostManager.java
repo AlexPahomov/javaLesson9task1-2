4936118d994566d29542262a39bdac5f38215d12
Alex Pahomov <renger29@gmail.com>
2021-02-02 20:30:56 +0300

    add manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, byte count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerID, int postID) {
+    }
+}
