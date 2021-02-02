50e349bd52e5ed703b44bcfcf98ccdfb2413f7bd
Alex Pahomov <renger29@gmail.com>
2021-02-02 20:22:06 +0300

    commit v2

--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -1,15 +1,37 @@
 package domain;
 
 public class Post {
-    private String profileName;
-    private String reportButton;
-    private String date;
-    private String postLogoURL;
+    private int id;
+    private int ownerID;
+    private int fromID;
+    private int createdBy;
+    private int date;
     private String mainText;
-    private String imageURL;
-    private String likesCount;
-    private String subscriberInfo
+    private int replyOwnerID;
+    private int replyPostID;
+    private boolean subscriberInfo;
     private String commentButton;
-    private String repostButton;
-    private String viewsCount;
+    private int commentCount;
+    private boolean commentCanPost;
+    private boolean commentCanClose;
+    private boolean commentCanOpen;
+    private String copyright;
+    private int likesCount;
+    private boolean userLikes;
+    private boolean canLike;
+    private boolean canPublish;
+    private int repostsCount;
+    private boolean userReposted;
+    private int viewsCount;
+    private String postType;
+    private String postSource;
+    private String geo;
+    private int signerID;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean pinned;
+    private boolean markedAsAd;
+    private boolean favorite;
+    private int postponedID;
     }
